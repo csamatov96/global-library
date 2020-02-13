2c4780b44fbd7b154e08fb71dcef054cc4600154
csamatov96 <csamatov96@gmail.com>
2020-02-12 19:53:21 -0600

    added

--- src/com/lib/App.groovy
+++ src/com/lib/App.groovy
@@ -6,30 +6,27 @@ name: 'APP_VERSION', trim: false)])])
 node {
     stage("Pull Repo"){
         git 'https://github.com/csamatov96/DockerFile-Python-.git'
-
     }
+
     stage("Build Image"){
-        sh "docker build -t app1:${APP_VERSION} ." //version parameter can be added instead 
-        
+        sh "docker build -t repo:${APP_VERSION} ." //version parameter can be added instead     
     }
 
     stage("Build Tag"){
-        sh '''docker tag app1:${APP_VERSION} 676918110389.dkr.ecr.us-east-1.amazonaws.com/repo:${APP_VERSION}''' //version parameter can be added instead 
-        
+        sh '''docker tag app1:${APP_VERSION} 3676918110389.dkr.ecr.us-east-1.amazonaws.com/repo:${APP_VERSION}''' //version parameter can be added instead 
     }
 
     stage("Login to ECR"){
-        sh '''$(aws ecr get-login --no-include-email --region us-east-1)'''
-        
+        sh '''$(aws ecr get-login --no-include-email --region us-east-1)'''        
     }
+
     stage("Push Image"){
         sh "docker images"
-        sh "docker push 676918110389.dkr.ecr.us-east-1.amazonaws.com/repo:${APP_VERSION}" //URI
-        
+        sh "docker push 3676918110389.dkr.ecr.us-east-1.amazonaws.com/repo:${APP_VERSION}" //URI    
     }
+
     stage("Notify"){
-        sh "New Image"
-        
+        sh "echo New Image"    
     }
 
 }
