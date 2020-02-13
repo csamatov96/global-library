3aa102dc78e753189a3d1fae11d66022f3c6d61d
csamatov96 <csamatov96@gmail.com>
2020-02-12 19:12:09 -0600

    added

---
+++ src/com/lib/App.groovy
@@ -0,0 +1,28 @@
+properties([[$class: 'JiraProjectProperty'],
+parameters([string(defaultValue: 'v1', 
+description: 'Please provide a version number ', 
+name: 'APP_VERSION', trim: false)])])
+
+node {
+    stage("Pull Repo"){
+        git 'https://github.com/csamatov96/DockerFile-Python-.git'
+
+    }
+    stage("Build / Tag Image"){
+        sh "docker build -t app_name:${APP_NAME} ." //version parameter can be added instead 
+        
+    }
+    stage("Login to ECR"){
+        sh "$(aws ecr get-login --no-include-email --region us-east-1)"
+        
+    }
+    stage("Push Image"){
+        sh "docker push app_name:${APP_NAME}"
+        
+    }
+    stage("Notify"){
+        
+        
+    }
+
+}
