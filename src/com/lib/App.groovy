202efb4fccf2336498d39cba9f5335d047e408f6
csamatov96 <csamatov96@gmail.com>
2020-02-12 19:29:16 -0600

    added

--- src/com/lib/App.groovy
+++ src/com/lib/App.groovy
@@ -8,16 +8,23 @@ node {
         git 'https://github.com/csamatov96/DockerFile-Python-.git'
 
     }
-    stage("Build / Tag Image"){
+    stage("Build Image"){
         sh "docker build -t app1:${APP_VERSION} ." //version parameter can be added instead 
         
     }
+
+    stage("Build Tag"){
+        sh '''docker tag app1:${APP_VERSION} 676918110389.dkr.ecr.us-east-1.amazonaws.com/repo/app1:${APP_VERSION}''' //version parameter can be added instead 
+        
+    }
+
     stage("Login to ECR"){
         sh '''$(aws ecr get-login --no-include-email --region us-east-1)'''
         
     }
     stage("Push Image"){
-        sh "docker push 676918110389.dkr.ecr.us-east-1.amazonaws.com/repo/app1:${APP_VERSION}" //URI
+        sh "docker images"
+        sh "docker push 676918110389.dkr.ecr.us-east-1.amazonaws.com/app1:${APP_VERSION}" //URI
         
     }
     stage("Notify"){
